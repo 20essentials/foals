2017040f3727b4cfbcfe38fbb1791374c6b732ed
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 17:24:54 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FOALS';
+export const nameSongs = [
+	"MOUNTAIN AT MY GATES", /*1*/
+	"MY NUMBER", /*2*/
+	"LATE NIGHT", /*3*/
+	"2AM", /*4*/
+	"UNDER THE RADAR", /*5*/
+	"2001", /*6*/
+	"LIFE IS YOURS", /*7*/
+	"THE SOUND", /*8*/
+	"FLUTTER", /*9*/
+	"CREST OF THE WAVE", /*10*/
+	"WAKE ME UP", /*11*/
+	"LOOKING HIGH", /*12*/
+	"WILD GREEN", /*13*/
+	"RED DESERT", /*14*/
+	"THE RUNNER", /*15*/
+	"WASH OFF", /*16*/
+	"BLACK BULL", /*17*/
+	"LIKE LIGHTNING", /*18*/
+	"IKARIA", /*19*/
+	"10,000 FEET", /*20*/
+]
